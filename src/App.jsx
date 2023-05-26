8a75f9de014f034f6a7c2da89d861c8614d1dae6
Kabina Thapa <thapakabina28@gmail.com>
2023-05-26 01:58:11 -0400

    More CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,50 @@
-import { useState } from 'react'
-import './App.css'
-import arrayelements from './index'
-import img1 from './kay.jpg'
-import img2 from './Ray.jpg'
-import img3 from './Qay.jpg'
-
+import { useState } from "react";
+import "./App.css";
+import arrayelements from "./index";
+import img1 from "./kay.jpg";
+import img2 from "./Ray.jpg";
+import img3 from "./Qay.jpg";
 
 function App() {
-  const profiles=[
-                {
-                  id:1,
-                  name:"Kay",
-                  age:25,
-                  hobbies:["Readings,","Gardening,", "and Cooking"],
-                  bio:"Kay is a passionate learner who enjoys exploring new technologies.",
-                  image:img1
-                },
-                {
-                  id:2,
-                  name:"Ray",
-                  age:20,
-                  hobbies:["Writing,","Painting,", "and Dog Walking"],
-                  bio:"Ray is a fun loving person who always finds people to party around.",
-                  image:img2
-                },
-                { 
-                  id:3,
-                  name:"Qay",
-                  age:22,
-                  hobbies:["Dancing,","Hiking,", "and Cooking"],
-                  bio:"Qay loves coding and is extremely shy.",
-                  image:img3
-                }
-  ]
-   const profileoutput= profiles.map(arrayelements)
+  const profiles = [
+    {
+      id: 1,
+      name: "Kay",
+      age: 25,
+      hobbies: ["Readings,", "Gardening,", "and Cooking"],
+      bio: "Kay is a passionate learner who enjoys exploring new technologies.",
+      image: img1,
+    },
+    {
+      id: 2,
+      name: "Ray",
+      age: 20,
+      hobbies: ["Writing,", "Painting,", "and Dog Walking"],
+      bio: "Ray is a fun loving person who always finds people to party around.",
+      image: img2,
+    },
+    {
+      id: 3,
+      name: "Qay",
+      age: 22,
+      hobbies: ["Dancing,", "Hiking,", "and Cooking"],
+      bio: "Qay loves coding and is extremely shy.",
+      image: img3,
+    },
+  ];
 
+  const profileoutput = profiles.map(arrayelements);
   return (
     <>
-    <div class="box">
-      <div class="outline">
-        <h1>Get To Know Our Characters</h1>
+      <div class="box">
+        <div class="outline">
+          <h1>Get To Know Our Characters</h1>
 
-       
-        {profileoutput}
-        
+          {profileoutput}
+        </div>
       </div>
-      </div>
-        
     </>
-  )
+  );
 }
 
-export default App
+export default App;
